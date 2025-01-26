73760dedfc203186238c893695ea0a6479f0c79f
chaitanya <73682988+HiIamChaitanya@users.noreply.github.com>
2025-01-26 09:20:29 +0530

    update

--- setup.sh
+++ setup.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+cd 
+
+# clone dotfile repo to $HOME
+
+git clone https://github.com/HiIamChaitanya/dotfiles.git --depth=1 
+
+cd dotfiles/ 
+
+# install dotfiles
+
+chmod +x install.sh
+
+ ./install.sh
+
+sudo dnf update -y --refresh
