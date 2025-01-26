3302e834739fdaf727ec1550570766437fd3a0d3
Chaitanya <73682988+HiIamChaitanya@users.noreply.github.com>
2025-01-27 02:57:25 +0530

    commit

--- gnome-themes/install-theme.sh
+++ gnome-themes/install-theme.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
+cd ~/dotfiles/gnome-themes 
+
 # Install Gnome theme 
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git --depth=1
+
+cd ~/dotfiles/gnome-themes/WhiteSur-gtk-theme 
+
+chmod +x install.sh
+
+./install.sh -c dark --shell -i fedora -h smaller -sf -a all -m -l -HD --round --darker
+
+chmod +x tweak.sh
+
+./tweaks.sh -F -c dark
+
+sudo flatpak override --filesystem=xdg-config/gtk-3.0 && sudo flatpak override --filesystem=xdg-config/gtk-4.0
+
+cd .. && rm -rf WhiteSur-gtk-theme
