571ef89b84093707b0f798a5f43394698b40f195
Chaitanya <73682988+HiIamChaitanya@users.noreply.github.com>
2025-03-27 19:17:33 +0530

    fixed error

--- setup.sh
+++ setup.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+# Function to check if the user's answer is 'yes'
+answer_is_yes() {
+    read -r user_answer
+    [[ "$user_answer" =~ ^([yY][eE][sS]|[yY])$ ]]
+}
+
+print_banner() {
+    cat << "EOF"
 ________          __    _____.__.__                 
 \______ \   _____/  |__/ ____\__|  |   ____   ______
  |    |  \ /  _ \   __\   __\|  |  | _/ __ \ /  ___/
  |    `   (  <_> )  |  |  |  |  |  |_\  ___/ \___ \ 
 /_______  /\____/|__|  |__|  |__|____/\___  >____  >
         \/                                \/     \/ 
+EOF
 
-
+}
 # Function to handle dotfiles directory
 handle_dotfiles_directory() {
     local dotfiles_dir="$HOME/dotfiles"
 
     if [ -d "$dotfiles_dir" ]; then
-        ask_for_confirmation "Dotfiles directory already exists. Do you want to overwrite it? (y/n)"
+         echo "Dotfiles directory already exists. Do you want to overwrite it? (y/n)"
         if answer_is_yes; then
             echo "Overwriting existing dotfiles directory..."
             # Compress existing dotfiles directory
@@ -27,14 +36,22 @@ handle_dotfiles_directory() {
     fi
 }
 
+# Function to clone the dotfiles repository
+clone_dotfiles_repo() {
+    git clone --depth=1 https://github.com/HiIamChaitanya/dotfiles.git ~/dotfiles
+}
+
+# Function to install dotfiles
+install_dotfiles() {
+    bash ~/dotfiles/install.sh
+}
+
 # Main script execution
 main() {
+    print_banner
     handle_dotfiles_directory
-    # clone dotfile repo to $HOME
-    git clone --depth=1 https://github.com/HiIamChaitanya/dotfiles.git ~/dotfiles
-
-    # install dotfiles
-    ~/dotfiles/install.sh
+    clone_dotfiles_repo
+    install_dotfiles
 }
 
 main
