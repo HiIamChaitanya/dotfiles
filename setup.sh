95f33a95409484e0649789154024b1a752f4b70b
Chaitanya <73682988+HiIamChaitanya@users.noreply.github.com>
2025-03-27 13:12:37 +0530

    changed install method and added backup function for dotfiles before installing

--- setup.sh
+++ setup.sh
@@ -7,8 +7,34 @@ ________          __    _____.__.__
 /_______  /\____/|__|  |__|  |__|____/\___  >____  >
         \/                                \/     \/ 
 
-# clone dotfile repo to $HOME
-git clone --depth=1 https://github.com/HiIamChaitanya/dotfiles.git ~/dotfiles
 
-# install dotfiles
-~/dotfiles/install.sh
+# Function to handle dotfiles directory
+handle_dotfiles_directory() {
+    local dotfiles_dir="$HOME/dotfiles"
+
+    if [ -d "$dotfiles_dir" ]; then
+        ask_for_confirmation "Dotfiles directory already exists. Do you want to overwrite it? (y/n)"
+        if answer_is_yes; then
+            echo "Overwriting existing dotfiles directory..."
+            # Compress existing dotfiles directory
+            tar -czvf dotfiles_backup_$(date +%Y%m%d%H%M%S).tar.gz "$dotfiles_dir"
+            echo "Backed up existing dotfiles to dotfiles_backup_$(date +%Y%m%d%H%M%S).tar.gz"
+            rm -rf "$dotfiles_dir"
+        else
+            echo "Keeping existing dotfiles directory. Exiting..."
+            exit 0
+        fi
+    fi
+}
+
+# Main script execution
+main() {
+    handle_dotfiles_directory
+    # clone dotfile repo to $HOME
+    git clone --depth=1 https://github.com/HiIamChaitanya/dotfiles.git ~/dotfiles
+
+    # install dotfiles
+    ~/dotfiles/install.sh
+}
+
+main
